6d9c0767060933556f37b92f17d52e64da1fdb0e
Yulin Zeng <1191220341@qq.com>
2022-02-24 12:02:28 -0800

    draft insert controller

--- mailServer/src/main/java/com/scu/group8/mailServer/controller/MailController.java
+++ mailServer/src/main/java/com/scu/group8/mailServer/controller/MailController.java
@@ -1,10 +1,30 @@
 package com.scu.group8.mailServer.controller;
 
+import com.scu.group8.mailServer.dto.DraftDto;
+import com.scu.group8.mailServer.services.DraftService;
+import com.scu.group8.mailServer.utils.Result;
+import com.scu.group8.mailServer.vo.DraftVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value = "/mail")
-public class MailController {
+import javax.servlet.http.HttpServletRequest;
 
+@RestController
+public class MailController {
+    public static final String SESSION_NAME = "userInfo";
+
+    @Autowired
+    private DraftService draftService;
+
+    @RequestMapping(value = "/draft/insert", method = RequestMethod.POST)
+    public Result<DraftVo> insertDraft(@RequestBody DraftDto draftDto, HttpServletRequest request){
+        Result result;
+        int userId = Integer.valueOf((Integer) request.getSession().getAttribute(SESSION_NAME));
+        draftDto.setMailId(userId);
+        result = draftService.insertDraft(draftDto);
+        return result;
+    }
 }
--- mailServer/src/test/java/com/scu/group8/mailServer/dao/MailMapperTest.java
+++ mailServer/src/test/java/com/scu/group8/mailServer/dao/MailMapperTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.SpringVersion;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.swing.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
