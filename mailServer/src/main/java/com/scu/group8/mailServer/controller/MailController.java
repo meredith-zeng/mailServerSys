6d83e1bd7fe5da1a8285614a7cc73d254450c13e
jmnian <87175647+jmnian@users.noreply.github.com>
2022-02-27 16:34:54 -0800

    Updated MailController.java
    Added insertDraft, sendMail, deleteInbox, and deleteOutbox

--- mailServer/src/main/java/com/scu/group8/mailServer/controller/MailController.java
+++ mailServer/src/main/java/com/scu/group8/mailServer/controller/MailController.java
@@ -1,7 +1,11 @@
 package com.scu.group8.mailServer.controller;
 
 import com.scu.group8.mailServer.dto.DraftDto;
+import com.scu.group8.mailServer.dto.MailDto;
 import com.scu.group8.mailServer.services.DraftService;
+import com.scu.group8.mailServer.services.InboxService;
+import com.scu.group8.mailServer.services.MailService;
+import com.scu.group8.mailServer.services.OutboxService;
 import com.scu.group8.mailServer.utils.Result;
 import com.scu.group8.mailServer.vo.DraftVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +22,43 @@ public class MailController {
 
     @Autowired
     private DraftService draftService;
+    private MailService mailService;
+    private InboxService inboxService;
+    private OutboxService outboxService;
 
-    @RequestMapping(value = "/draft/insert", method = RequestMethod.POST)
+    //Draft Email
+    @RequestMapping(value = "mail/draft", method = RequestMethod.POST)
     public Result<DraftVo> insertDraft(@RequestBody DraftDto draftDto, HttpServletRequest request){
         Result result;
-        int userId = Integer.valueOf((Integer) request.getSession().getAttribute(SESSION_NAME));
-        draftDto.setMailId(userId);
+        int ownerId = Integer.valueOf((Integer) request.getSession().getAttribute(SESSION_NAME));
+        draftDto.setOwnerId(ownerId);
         result = draftService.insertDraft(draftDto);
+        return result;
+    }
+
+    //Send Email
+    @RequestMapping(value = "mail/send", method = RequestMethod.POST)
+    public Result sendMail(@RequestBody MailDto mailDto, HttpServletRequest request) {
+        Result result;
+        int senderId = Integer.valueOf((Integer) request.getSession().getAttribute(SESSION_NAME));
+        mailDto.setSenderId(senderId);
+        result = mailService.sendMail(mailDto);
+        return result;
+    }
+
+    //Delete Inbox Email
+    @RequestMapping(value = "/mail/deleteInbox", method = RequestMethod.POST)
+    public Result deleteInboxMail(@RequestBody MailDto mailDto, HttpServletRequest request) {
+        Result result;
+        result = inboxService.deleteInboxMail(mailDto.getMailId());
+        return result;
+    }
+
+    //Delete Outbox Email
+    @RequestMapping(value = "/mail/deleteOutbox", method = RequestMethod.POST)
+    public Result deleteOutboxMail(@RequestBody MailDto mailDto, HttpServletRequest request) {
+        Result result;
+        result = outboxService.deleteOutboxMail(mailDto.getMailId());
         return result;
     }
 }
